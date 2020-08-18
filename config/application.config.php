3e79830a5f5f8688a1d20e24b957554337b225f0
cylmat <freeonisland@yahoo.fr>
2020-08-18 20:49:56 +0200

    set application event debugger

--- config/application.config.php
+++ config/application.config.php
@@ -1,5 +1,8 @@
 <?php
 
+use Interop\Container\ContainerInterface;
+use LmConsole\Command\DebugEventsModel\EventDebuggerManager;
+
 /**
  * If you need an environment-specific system or application configuration,
  * there is an example in the documentation
@@ -7,11 +10,24 @@
  * @see https://docs.laminas.dev/tutorials/advanced-config/#environment-specific-application-configuration
  */
 return [
-    'modules' => ['Laminas\Router','LmConsole'],
+    'modules' => [
+        'Laminas\Router',
+        'LmConsole'
+    ],
+
     'module_listener_options' => [
         'use_laminas_loader' => false,
         'config_glob_paths' => [
             realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'
         ]
+    ],
+    'service_manager' =>  [
+        'factories'  => [
+            'EventManager' => function (ContainerInterface $container, $name, array $options = null) {
+                $shared = $container->has('SharedEventManager') ? $container->get('SharedEventManager') : null;
+                return new EventDebuggerManager($shared);
+            }
+           // 'ServiceListener'         => ServiceListenerFactory::class,
+        ],
     ]
 ];
